0d7952849aa58c941da9a8b7c1cfb7536bf1bcce
doug y'barbo <doug.ybarbo@gmail.com>
2012-12-04 11:23:50 -0800

    added simple api template for tornadio api

---
+++ app-tn1.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python2.7
+# encoding: utf-8
+
+
+import tornado.ioloop
+import tornado.web
+
+from tapioca import TornadoRESTful, ResourceHandler
+
+class HelloResource(ResourceHandler):
+
+    def get_collection(self, callback):
+        callback("Hello, world!")
+
+api = TornadoRESTful(discovery=True)
+api.add_resource('hello', HelloResource)
+application = tornado.web.Application(
+    api.get_url_mapping()
+)
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
