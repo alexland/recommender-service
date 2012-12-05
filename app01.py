de67cc216ae20a7f38a5d3c7b01a798822098747
doug y'barbo <doug.ybarbo@gmail.com>
2012-12-04 19:29:00 -0800

    simplified to a minimum template

--- app01.py
+++ app01.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -10,4 +11,4 @@ def hello():
 	return "Hello World!"
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
