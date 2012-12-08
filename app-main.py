57106346eb182709c8183ff26e87c6bfb11c1633
doug y'barbo <doug.ybarbo@gmail.com>
2012-12-07 20:49:14 -0800

    removed jaccard index code & added import line

--- app-main.py
+++ app-main.py
@@ -24,6 +24,7 @@ from flask import request
 from contextlib import closing
 from redis import StrictRedis as redis
 import numpy as NP
+import jaccard as REC
 
 DATABASE = 0
 DEBUG = True
@@ -45,39 +46,9 @@ def connect_db():
 def before_request():
 	g.db = connect_db()
 
-
 # @app.teardown_request
 # def teardown_request(exception):
 # 	g.db.close()
-
-
-def jaccard_prep(bitstr):
-	return NP.array( list(bitstr), dtype=int )
-
-
-def jaccard(vec1, vec2) :
-    """
-        returns Jaccard Index for 2 bit-wise vectors;
-        pass in 2 x NumPy 1D arrays (type is int/float/boolean, 
-		if former 2;
-        values of 0, 1 only ) of *equal* size
-    """
-    vec1, vec2 = NP.squeeze(vec1), NP.squeeze(vec2)
-    fnx = lambda v : NP.array(v, dtype=bool)
-    vec1, vec2 = fnx(vec1), fnx(vec2)
-    try :
-        numer = NP.sum(vec1 == vec2)
-    except ValueError :
-        print("check that the two vectors are of equal length")
-    denom = float(vec1.size)
-    return numer / denom
-
-
-def recommendations(user_id, num_recs=10):
-	vec1 = jaccard_prep(g.db.get(user_id))
-	all_vecs = [ [jaccard_prep(g.db.get(k)), int(k)] for k in g.db.keys('*') ]
-	return sorted([[jaccard(vec1, row[0]), row[1]] for row in all_vecs],
-		reverse=True)[1:][:num_recs]
 
 
 @app.route('/jaccard')
@@ -86,7 +57,7 @@ def api_jaccard():
 		return "no valid user id supplied"
 	else:	 
 		k1 = request.args['user_id']
-		ji = recommendations(k1)
+		ji = REC.recommendations(k1)
 		r = FK.jsonify(recs=ji)
 		r.status_code = 200
 		return r
