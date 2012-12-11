fee04875ff0d51700c148c968254491643ff1e94
doug y'barbo <doug.ybarbo@gmail.com>
2012-12-10 19:21:16 -0800

    debug

--- jaccard.py
+++ jaccard.py
@@ -7,11 +7,7 @@ from redis import StrictRedis as redis
 import numpy as NP
 
 
-def jaccard_prep(bitstr):
-	return NP.array( list(bitstr), dtype=int )
-
-
-def jaccard(vec1, vec2) :
+def recommender(vec1, vec2) :
     """
         returns Jaccard Index for 2 bit-wise vectors;
         pass in 2 x NumPy 1D arrays (type is int/float/boolean, 
@@ -28,9 +24,3 @@ def jaccard(vec1, vec2) :
     denom = float(vec1.size)
     return numer / denom
 
-
-def recommendations(user_id, num_recs=10):
-	vec1 = jaccard_prep(g.db.get(user_id))
-	all_vecs = [ [jaccard_prep(g.db.get(k)), int(k)] for k in g.db.keys('*') ]
-	return sorted([[jaccard(vec1, row[0]), row[1]] for row in all_vecs],
-		reverse=True)[1:][:num_recs]
